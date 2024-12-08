81a2610e106106188072bf774cbfd3a05568ef15
Nitesh <kagnitesh@gmail.com>
2024-12-08 11:59:27 +0530

    HomePageThaliUPdates

--- frontend/src/components/SpecialDishSection.jsx
+++ frontend/src/components/SpecialDishSection.jsx
@@ -3,7 +3,11 @@ import ThaliCard from "./ThaliCard";
 import SectionHeader from "./SectionHeader";
 
 const SpecialDishSection = ( {className} ) => {
-  const ThaliPic = "/thaliPic.png"; // Ensure the path is correct based on your project structure
+  const dasheraThaliPic = "/thaliPics/dasheraThaliPic.png"; 
+  const GaneshChaturthiPic = "/thaliPics/GaneshChaturthiPic.png";
+  const makarsankrantiPic = "/thaliPics/makarsankrantiPic.png";
+  const navaratriPic = "/thaliPics/navaratriPic.png";
+
   const description =
     "The state-of-the-art facility has automated machinery, is installed with rust-free pipelines and faucets and uses only RO treated water.";
 
@@ -19,33 +23,33 @@ const SpecialDishSection = ( {className} ) => {
           <div className="flex md:grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-6 xl:gap-10 overflow-x-auto md:overflow-visible mx-0 md:mx-16 lg:mx-16 xl:mx-56 -mt-24">
             <div className="">
               <ThaliCard
-                title="Rajasthan Thali"
+                title="Dashera Special"
                 price="₹ 104"
-                imageSrc={ThaliPic}
+                imageSrc={dasheraThaliPic}
                 ratingCount={4}
               />
             </div>
             <div className="">
               <ThaliCard
-                title="Gujarati Thali"
+                title="Ganesh Chaturthi Special"
                 price="₹ 104"
-                imageSrc={ThaliPic}
+                imageSrc={GaneshChaturthiPic}
                 ratingCount={3}
               />
             </div>
             <div className="">
               <ThaliCard
-                title="Bengali Thali"
+                title="Makarsankranti Special"
                 price="₹ 104"
-                imageSrc={ThaliPic}
+                imageSrc={makarsankrantiPic}
                 ratingCount={5}
               />
             </div>
             <div className="">
               <ThaliCard
-                title="Bengali Thali"
+                title="Navaratri Special"
                 price="₹ 104"
-                imageSrc={ThaliPic}
+                imageSrc={navaratriPic}
                 ratingCount={5}
               />
             </div>
